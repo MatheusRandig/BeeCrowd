bc376e878fe3e7d1ea52613c99e9422f54f79b3a
Matheus Randig <matheus.randig@gmail.com>
2024-09-03 21:17:05 -0300

    Unit test update

--- Python/Problems/beecrowd_1001.py
+++ Python/Problems/beecrowd_1001.py
@@ -8,9 +8,11 @@ def beginner1001(x,y):
 def beginner1001_print(x,y):
     return(f"X = {beginner1001(x,y)}")
 
-# input values from user
-x = int(input())
-y = int(input())
 
-#print result
-print(beginner1001_print(x,y))
+if __name__ == "__main__":
+    # input values from user
+    x = int(input())
+    y = int(input())
+
+    #print result
+    print(beginner1001_print(x,y))
--- Python/Tests/test_beecrowd_1001.py
+++ Python/Tests/test_beecrowd_1001.py
@@ -16,13 +16,13 @@ def test_beginner1001_three():
 
 #Checking output
 def test_beginner1001_print_one():
-    result = beecrowd_1001.beginner1001(10,9)
+    result = beecrowd_1001.beginner1001_print(10,9)
     assert result == "X = 19"
 
 def test_beginner1001_print_two():
-    result = beecrowd_1001.beginner1001(-10,4)
+    result = beecrowd_1001.beginner1001_print(-10,4)
     assert result == "X = -6"
 
 def test_beginner1001_print_three():
-    result = beecrowd_1001.beginner1001(15,-7)
+    result = beecrowd_1001.beginner1001_print(15,-7)
     assert result == "X = 8"
