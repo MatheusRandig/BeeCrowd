cb8f0e38c3943d6293ef0cfa6826c350e1a75bed
Matheus Randig <matheus.randig@gmail.com>
2024-09-04 16:17:36 -0300

    pytest configuration for CI

--- Python/Problems/beecrowd_1002.py
+++ Python/Problems/beecrowd_1002.py
@@ -1,12 +1,12 @@
 # https://judge.beecrowd.com/en/problems/view/1002
 
 # Circle area
-def beginner1002_(x):
+def beginner1002_circleArea(x):
     return x*x*3.14159
 
 # print result
-def beginner1002_print(x,y):
-    return(f"X = {beginner1002(x,y)}")
+def beginner1002_print(x):
+    return(f"X = {beginner1002_circleArea(x)}")
 
 
 if __name__ == "__main__":
