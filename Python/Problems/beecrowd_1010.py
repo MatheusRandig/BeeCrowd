6459263c8cdb43fe283c4d7abdb198617d6bb300
Matheus Randig <matheus.randig@gmail.com>
2024-09-05 17:02:57 -0300

    1010 comments

--- Python/Problems/beecrowd_1010.py
+++ Python/Problems/beecrowd_1010.py
@@ -1,13 +1,13 @@
 #https://judge.beecrowd.com/en/problems/view/1010
 
-
+#Returns the total price of a product, the product of quantity and value
 def total_price(value, quantity):
     return value*quantity
 
 
+#Returns the correct output for the exercise
 def output_print(p1,q1,p2,q2):
     return "VALOR A PAGAR: R$ "+'{:.2f}'.format(total_price(p1,q1)+total_price(p2,q2))
-
 
 
 #user input
