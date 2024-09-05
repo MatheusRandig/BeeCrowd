7a011c0937a2466f77203d27daf9cdb1e9026138
Matheus Randig <matheus.randig@gmail.com>
2024-09-05 16:32:50 -0300

    1008

---
+++ Python/Problems/beecrowd_1009.py
@@ -0,0 +1,21 @@
+#https://judge.beecrowd.com/en/problems/view/1009
+
+
+#Returns a float of the total salary
+#The total is fixed salary + 15% of commissions of the monthly sell
+def total_salary(fixed_salary, month_sells):
+    return '{:.2f}'.format(fixed_salary+(month_sells*0.15))
+
+
+#output salary
+def output_print(salary, sells):
+    return "TOTAL = R$ "+str(total_salary(salary, sells))
+
+#user input
+if __name__ == "__main__":
+
+    name = input()
+    salary =float(input())
+    sells = float(input())
+
+    print(output_print(salary,sells))
