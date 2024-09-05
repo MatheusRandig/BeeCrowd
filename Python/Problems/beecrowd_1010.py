1d84f1b5289c979edcf072aaec6b0cb06f49a59e
Matheus Randig <matheus.randig@gmail.com>
2024-09-05 17:00:28 -0300

    1010

---
+++ Python/Problems/beecrowd_1010.py
@@ -0,0 +1,19 @@
+#https://judge.beecrowd.com/en/problems/view/1010
+
+
+def total_price(value, quantity):
+    return value*quantity
+
+
+def output_print(p1,q1,p2,q2):
+    return "VALOR A PAGAR: R$ "+'{:.2f}'.format(total_price(p1,q1)+total_price(p2,q2))
+
+
+
+#user input
+if __name__ == "__main__":
+
+    product1 = [float(x) for x in input().split()]
+    product2 = [float(x) for x in input().split()]
+
+    print(output_print(product1[2],product1[1],product2[2],product2[1]))
