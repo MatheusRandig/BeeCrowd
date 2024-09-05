fd7bd3857cdf8f7f415a80f1e2da599eb30c539b
Matheus Randig <matheus.randig@gmail.com>
2024-09-05 18:44:39 -0300

    1011

---
+++ Python/Problems/beecrowd_1011.py
@@ -0,0 +1,16 @@
+#https://judge.beecrowd.com/en/problems/view/1011
+
+#Returns Volume of the Circle
+def volume_circle(radius, decimal):
+    return ('{:.'+str(decimal)+'f}').format((radius**3)*3.14159*4/3)
+
+#Returns Exercise output
+def output_print(r):
+    return "VOLUME = "+str(volume_circle(r,3))
+
+# user input
+if __name__ == "__main__":
+
+    r = float(input())
+
+    print(output_print(r))
