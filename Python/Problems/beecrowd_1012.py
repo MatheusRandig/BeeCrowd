5bb7a86e431480e588363c23c8770c5cdf8e57c1
Matheus Randig <matheus.randig@gmail.com>
2024-09-05 20:09:28 -0300

    1012

---
+++ Python/Problems/beecrowd_1012.py
@@ -0,0 +1,46 @@
+#https://judge.beecrowd.com/en/problems/view/1012
+
+#Using sides, Returns the area of a rectangle triangle
+#digits is the accuracy of the float
+def area_rectangle_triangle(side_a,side_b,digits):
+    return ('{:.' + str(digits) + 'f}').format(side_a * side_b * 0.5)
+
+#Using radius, Returns the area of a circle
+#digits is the accuracy of the float
+def area_circle(r,digits):
+    return ('{:.' + str(digits) + 'f}').format(r * r * 3.14159)
+
+
+#Using bases and height, Returns the area of a trapezium
+#digits is the accuracy of the float
+def area_trapezium(base_a,base_b,height,digits):
+    return ('{:.' + str(digits) + 'f}').format((base_a + base_b )/2 * height)
+
+
+#Using side, Returns the area of a square
+#digits is the accuracy of the float
+def area_square(side, digits):
+    return ('{:.'+str(digits)+'f}').format(side * side)
+
+
+#Using sides, Returns the area of a rectangle
+#digits is the accuracy of the float
+def area_rectangle(side_a,side_b,digits):
+    return ('{:.' + str(digits) + 'f}').format(side_a * side_b)
+
+
+#Returns the exercise output
+def output_print(a,b,c):
+    return (f"TRIANGULO: {area_rectangle_triangle(a, c,3)}\n"
+            f"CIRCULO: {area_circle(c,3)}\n"
+            f"TRAPEZIO: {area_trapezium(a,b,c,3)}\n"
+            f"QUADRADO: {area_square(b,3)}\n"
+            f"RETANGULO: {area_rectangle(a,b,3)}")
+
+
+# user input
+if __name__ == "__main__":
+
+    v = [float(x) for x in input().split()]
+
+    print(output_print(v[0], v[1], v[2]))
---
+++ Python/Tests/test_beecrowd_1012.py
@@ -0,0 +1,61 @@
+from Python.Problems import beecrowd_1012
+
+# check area rectangle triangle
+def test_beginner1012_area_rectangle_triangle_one():
+    result = beecrowd_1012.area_rectangle_triangle(3.0,5.2,3)
+    assert result == '7.800'
+
+def test_beginner1012_area_rectangle_triangle_two():
+    result = beecrowd_1012.area_rectangle_triangle(12.7,15.2, 3)
+    assert result == '96.520'
+
+
+# check area circle
+def test_beginner1012_area_circle_one():
+    result = beecrowd_1012.area_circle(5.2,3)
+    assert result == '84.949'
+
+def test_beginner1012_area_circle_two():
+    result = beecrowd_1012.area_circle(15.2,3)
+    assert result == '725.833'
+
+
+# check area trapezium
+def test_beginner1012_area_trapezium_one():
+    result = beecrowd_1012.area_trapezium(3.0,4.0,5.2,3)
+    assert result == '18.200'
+
+def test_beginner1012_area_trapezium_two():
+    result = beecrowd_1012.area_trapezium(12.7,10.4,15.2,3)
+    assert result == '175.560'
+
+
+# check area square
+def test_beginner1012_area_square_one():
+    result = beecrowd_1012.area_square(4.0,3)
+    assert result == '16.000'
+
+def test_beginner1012_area_square_two():
+    result = beecrowd_1012.area_square(10.4,3)
+    assert result == '108.160'
+
+
+# check area rectangle
+def test_beginner1012_area_rectangle_one():
+    result = beecrowd_1012.area_rectangle(3.0,4.0,3)
+    assert result == '12.000'
+
+def test_beginner1012_area_rectangle_two():
+    result = beecrowd_1012.area_rectangle(12.7,10.4,3)
+    assert result == '132.080'
+
+
+#check exercise output
+def test_beginner1012_print_one():
+    result = beecrowd_1012.output_print(3.0,4.0,5.2)
+    assert result == "TRIANGULO: 7.800\nCIRCULO: 84.949\nTRAPEZIO: 18.200\nQUADRADO: 16.000\nRETANGULO: 12.000"
+
+def test_beginner1012_print_two():
+    result = beecrowd_1012.output_print(12.7,10.4,15.2)
+    assert result == "TRIANGULO: 96.520\nCIRCULO: 725.833\nTRAPEZIO: 175.560\nQUADRADO: 108.160\nRETANGULO: 132.080"
+
