8343bbf7b9b75f16174573f41b5f5779bbd0d373
Matheus Randig <matheus.randig@gmail.com>
2024-09-06 15:58:06 -0300

    1010 comments

---
+++ Python/Tests/test_beecrowd_1013.py
@@ -0,0 +1,21 @@
+from Python.Problems import beecrowd_1013
+
+# check area rectangle triangle
+def test_beginner1013_biggest_int_number_oneliner_one():
+    result = beecrowd_1013.biggest_int_number_oneliner("7 14 106")
+    assert result == 106
+
+def test_beginner1013_biggest_int_number_oneliner_two():
+    result = beecrowd_1013.biggest_int_number_oneliner("217 14 6")
+    assert result == 217
+
+
+#check exercise output
+def test_beginner1013_print_one():
+    result = beecrowd_1013.output_print("7 14 106")
+    assert result == "106 eh o maior"
+
+def test_beginner1013_print_two():
+    result = beecrowd_1013.output_print("217 14 6")
+    assert result == "217 eh o maior"
+
