68eb575904db4405b4dbf35e8e4fe05b4094ee2c
Matheus Randig <matheus.randig@gmail.com>
2024-09-09 16:59:01 -0300

    1016

---
+++ Python/Problems/beecrowd_1016.py
@@ -0,0 +1,19 @@
+#https://judge.beecrowd.com/en/problems/view/1016
+
+
+# Returns the minutes that takes for two cars in different speeds, v2 (bigger) v1 (smaller), to reach set distance (d)
+def distance_calc(v1,v2,d):
+    return d/((v2-v1)/60)
+
+
+# Return the exercise output
+def output_print(distance):
+    return str(int(distance_calc(60,90,distance)))+" minutos"
+
+
+# user input
+if __name__ == "__main__":
+
+    x = int(input())
+
+    print(output_print(x))
---
+++ Python/Tests/test_beecrowd_1016.py
@@ -0,0 +1,30 @@
+from Python.Problems import beecrowd_1016
+
+# check area rectangle triangle
+def test_beginner1016_distance_calc_one():
+    result = beecrowd_1016.distance_calc(60,90,30)
+    assert result == 60.0
+
+def test_beginner1016_distance_calc_two():
+    result = beecrowd_1016.distance_calc(60,90,110)
+    assert result == 220.0
+
+def test_beginner1016_distance_calc_three():
+    result = beecrowd_1016.distance_calc(60,90,7)
+    assert result == 14.0
+
+
+
+# check exercise output
+def test_beginner1016_print_one():
+    result = beecrowd_1016.output_print(30)
+    assert result == "60 minutos"
+
+def test_beginner1016_print_two():
+    result = beecrowd_1016.output_print(110)
+    assert result == "220 minutos"
+
+def test_beginner1016_print_three():
+    result = beecrowd_1016.output_print(7)
+    assert result == "14 minutos"
+
