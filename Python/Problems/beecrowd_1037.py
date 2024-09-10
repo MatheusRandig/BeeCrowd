23a5f48c37078326fb0d2ed57287745af9345021
Matheus Randig <matheus.randig@gmail.com>
2024-09-10 14:17:31 -0300

    1037

---
+++ Python/Problems/beecrowd_1037.py
@@ -0,0 +1,22 @@
+#https://judge.beecrowd.com/en/problems/view/1037
+
+#returns the interval of the float number
+def interval(n):
+    if 0 <= n <= 25:
+        result = "Intervalo [0,25]"
+    elif 25 < n <= 50:
+        result = "Intervalo (25,50]"
+    elif 50 < n <= 75:
+        result = "Intervalo (50,75]"
+    elif 75 < n <=100:
+        result = "Intervalo (75,100]"
+    else:
+        result = "Fora de intervalo"
+    return result
+
+
+#user input
+if __name__ == "__main__":
+
+    x = float(input())
+    print(interval(x))
---
+++ Python/Tests/test_beecrowd_1037.py
@@ -0,0 +1,19 @@
+from Python.Problems import beecrowd_1037
+
+
+# check exercise logic and output
+def test_beginner1037_interval_one():
+    result = beecrowd_1037.interval(25.01)
+    assert result == "Intervalo (25,50]"
+
+def test_beginner1037_interval_two():
+    result = beecrowd_1037.interval(25.00)
+    assert result == "Intervalo [0,25]"
+
+def test_beginner1037_interval_three():
+    result = beecrowd_1037.interval(100.00)
+    assert result == "Intervalo (75,100]"
+
+def test_beginner1037_interval_four():
+    result = beecrowd_1037.interval(-25.02)
+    assert result == "Fora de intervalo"
