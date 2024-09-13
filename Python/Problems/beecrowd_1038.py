c32915deababc992bf0824644b5ad630194ed84a
Matheus Randig <matheus.randig@gmail.com>
2024-09-13 15:26:49 -0300

    1038

---
+++ Python/Problems/beecrowd_1038.py
@@ -0,0 +1,31 @@
+#https://judge.beecrowd.com/en/problems/view/1038
+
+# Menu with prices of each code
+def snack_menu(code, quantity):
+    match code:
+        case 1:
+            value = 4.00
+        case 2:
+            value = 4.50
+        case 3:
+            value = 5.00
+        case 4:
+            value = 2.00
+        case 5:
+            value = 1.50
+        case _:
+            value = 0.00
+
+    return value*quantity
+
+#returns the output of the exercise
+def output_print(x,y):
+    return "Total: R$ "+str('{:.2f}'.format(snack_menu(x,y)))
+
+
+#User input
+if __name__ == "__main__":
+
+    x,y = [float(x) for x in input().split()]
+
+    print(output_print(x,y))
---
+++ Python/Tests/test_beecrowd_1038.py
@@ -0,0 +1,29 @@
+from Python.Problems import beecrowd_1038
+
+
+# check exercise snack menu
+def test_beginner1038_snack_menu_one():
+    result = beecrowd_1038.snack_menu(3, 2)
+    assert result == 10.00
+
+def test_beginner1038_snack_menu_two():
+    result = beecrowd_1038.snack_menu(4, 3)
+    assert result == 6.00
+
+def test_beginner1038_snack_menu_three():
+    result = beecrowd_1038.snack_menu(2, 3)
+    assert result == 13.50
+
+
+# check exercise snack menu
+def test_beginner1038_output_print_one():
+    result = beecrowd_1038.output_print(3, 2)
+    assert result == "Total: R$ 10.00"
+
+def test_beginner1038_output_print_two():
+    result = beecrowd_1038.output_print(4, 3)
+    assert result == "Total: R$ 6.00"
+
+def test_beginner1038_output_print_three():
+    result = beecrowd_1038.output_print(2, 3)
+    assert result == "Total: R$ 13.50"
