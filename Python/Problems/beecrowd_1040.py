264b90afece6685ebd628bfca577cb9674d20e60
Matheus Randig <matheus.randig@gmail.com>
2024-09-14 11:07:20 -0300

    Test error commit and push, Ruff and Mypy

--- Python/Problems/beecrowd_1040.py
+++ Python/Problems/beecrowd_1040.py
@@ -1,2 +1,41 @@
 # https://judge.beecrowd.com/en/problems/view/1040
 
+# Returns a float of the average notes of a student
+def average_student(n1: float, n2:float, n3: float, n4: float, decimal: int) -> float:
+    return round(((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10, decimal)
+
+# Calculate if a student is approved or failed after exam
+def recu_student(average: float,  exam: float, decimal: int) -> str:
+    if (average + exam) / 2 >= 5:
+        result = "\nAluno aprovado."
+    else:
+        result = "\nAluno reprovado."
+    result +="\nMedia final: "+str(round((average + exam) / 2, decimal))
+    return result
+
+
+#Print exercise output as intended
+def output_print(n1: float, n2: float, n3: float, n4: float) -> str:
+    average = average_student(n1, n2, n3, n4, 1)
+    result="Media: "+str(average)
+
+    if average >= 7:
+        result += "\nAluno aprovado."
+
+    elif average < 5.0:
+        result += "\nAluno reprovado."
+
+    else:
+        result += "\nAluno em exame."
+        exam = float(input())
+        result += "\nNota do exame: "+f"{exam:.1f}"
+        result += recu_student(average, exam, 1)
+
+    return result
+
+if __name__ == "__main__":
+
+    v = [float(x) for x in input().split()]
+
+    print(output_print(v[0], v[1], v[2], v[3]))
+
---
+++ Python/Tests/test_beecrowd_1040.py
@@ -0,0 +1,48 @@
+from Python.Problems import beecrowd_1040
+
+
+# check student average function
+def test_beginner1040_average_student_one() -> None:
+    result = beecrowd_1040.average_student(2.0 , 4.0, 7.5, 8.0, 1)
+    assert result == 5.4
+
+
+def test_beginner1040_average_student_two() -> None:
+    result = beecrowd_1040.average_student(2.0, 6.5, 4.0, 9.0, 1)
+    assert result == 4.8
+
+
+def test_beginner1040_average_student_three() -> None:
+    result = beecrowd_1040.average_student(9.0, 4.0, 8.5, 9.0, 1)
+    assert result == 7.3
+
+
+# check recu student function with approved and failed
+def test_beginner1040_recu_student_one() -> None:
+    result = beecrowd_1040.recu_student(5.4, 6.4, 1)
+    assert result == "\nAluno aprovado.\nMedia final: 5.9"
+
+
+def test_beginner1040_recu_student_two() -> None:
+    result = beecrowd_1040.recu_student(5.4, 0, 1)
+    assert result == "\nAluno reprovado.\nMedia final: 2.7"
+
+
+
+# check desired output
+def test_beginner1040_output_print_one(monkeypatch) -> None:
+    monkeypatch.setattr('builtins.input', lambda: 6.4)
+    result = beecrowd_1040.output_print(2.0 , 4.0, 7.5, 8.0)
+    assert result == "Media: 5.4\nAluno em exame.\nNota do exame: 6.4\nAluno aprovado.\nMedia final: 5.9"
+
+
+def test_beginner1040_output_print_two() -> None:
+    result = beecrowd_1040.output_print(2.0, 6.5, 4.0, 9.0)
+    assert result == "Media: 4.8\nAluno reprovado."
+
+
+def test_beginner1040_output_print_three() -> None:
+    result = beecrowd_1040.output_print(9.0, 4.0, 8.5, 9.0)
+    assert result == "Media: 7.3\nAluno aprovado."
+
+
