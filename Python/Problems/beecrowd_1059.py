dd8e8a6aeb83d54605136fb0e2397f4b5d59e31d
Matheus Randig <matheus.randig@gmail.com>
2025-05-16 16:18:41 -0700

    This is a VSCODE test

--- Python/Problems/beecrowd_1059.py
+++ Python/Problems/beecrowd_1059.py
@@ -6,8 +6,7 @@ def zero_to_hundred() -> str:
     for i in range(49):
         result += str((i + 1) * 2) + "\n"
     return result + "100"
-
-
+#test 
 # Exercise call and print
 if __name__ == "__main__":
     print(zero_to_hundred())
