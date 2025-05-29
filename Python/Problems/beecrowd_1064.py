5abd25e980ba10005b8a6a76183876d4d0bcb25c
Matheus Randig <Matheus.randig@gmail.com>
2025-05-28 22:00:59 -0300

    1064

---
+++ Python/Problems/beecrowd_1064.py
@@ -0,0 +1,33 @@
+# https://judge.beecrowd.com/en/problems/view/1064
+
+# Given a list returns a tuple with (total, count of positives)
+# The function accpets previous values to total and positives numbers, to continue a given average.
+def average_of_positives(values: list[float], positives_count: int, total: float) -> float:
+    for i in values:
+        if i > 0:
+            positives_count = positives_count + 1
+            total = total + i
+    return total, positives_count
+
+
+# Format to excersise
+def output_1064(list_input: float) -> str:
+    result = average_of_positives(list_input, 0, 0)
+    if result[1] == 0:
+        return "Sem Valores Positivos"
+    else:
+        return str(result[1]) + " valores positivos\n" + f"{(result[0] / result[1]):.1f}"
+
+
+# Exercise call and print
+if __name__ == "__main__":
+    list_input = [
+        float(input()),
+        float(input()),
+        float(input()),
+        float(input()),
+        float(input()),
+        float(input()),
+    ]
+
+    print(output_1064(list_input))
---
+++ Python/Tests/test_beecrowd_1064.py
@@ -0,0 +1,23 @@
+from Python.Problems import beecrowd_1064
+
+
+# check average_of_positives of exercise 1064
+def test_beginner1064_average_of_positives_one() -> None:
+    result = beecrowd_1064.average_of_positives([7, -5, 6, -3.4, 4.6, 12], 0, 0)
+    assert result == (29.6, 4)
+
+
+def test_beginner1064_average_of_positives_two() -> None:
+    result = beecrowd_1064.average_of_positives([1, 1, 1, 1, 1, 1], 5, 5)
+    assert result == (11, 11)
+
+
+# ocheck output_1064 of ecercise 1064
+def test_beginner1064_output_1064_one() -> None:
+    result = beecrowd_1064.output_1064([7, -5, 6, -3.4, 4.6, 12])
+    assert result == "4 valores positivos\n7.4"
+
+
+def test_beginner1064_output_1064_two() -> None:
+    result = beecrowd_1064.output_1064([-7, -5, -6, -3.4, -4.6, -12])
+    assert result == "Sem Valores Positivos"
