65043133ed17fda4cd0baa2f75c10823f967b10b
Matheus Randig <Matheus.randig@gmail.com>
2025-08-01 18:11:00 -0300

    1070

---
+++ Python/Problems/beecrowd_1070.py
@@ -0,0 +1,32 @@
+# https://judge.beecrowd.com/en/problems/view/1067
+
+# Given int n return a int list of next 6 odd numbers
+def next_six_odd(n: int) -> list[int]:
+    next_six_odd = []
+
+    if n % 2 == 0:
+        n += 1
+    for i in range(6):
+        next_six_odd.append(n + i + i)
+
+    return next_six_odd
+
+
+# Format to excersive expected output
+def output_1070(n: int) -> str:
+    odd_list = next_six_odd(n)
+
+    result = ""
+    for i in odd_list:
+        result += str(i) + "\n"
+
+    # remove last \n"
+    result = result[:-1]
+    return result
+
+
+# Exercise call and print
+if __name__ == "__main__":
+    n = int(input())
+
+    print(output_1070(n))
---
+++ Python/Tests/test_beecrowd_1070.py
@@ -0,0 +1,23 @@
+from Python.Problems import beecrowd_1070
+
+
+# check aodd_zero_to_range of exercise 1067
+def test_beginner1070_next_six_odd_one() -> None:
+    result = beecrowd_1070.next_six_odd(8)
+    assert result == ([9, 11, 13, 15, 17, 19])
+
+
+def test_beginner1070_next_six_odd_two() -> None:
+    result = beecrowd_1070.next_six_odd(1)
+    assert result == ([1, 3, 5, 7, 9, 11])
+
+
+# check output_1070 in exercise 1070
+def test_beginner1070_output_1070_one() -> None:
+    result = beecrowd_1070.output_1070(8)
+    assert result == ("9\n11\n13\n15\n17\n19")
+
+
+def test_beginner1070_output_1070_two() -> None:
+    result = beecrowd_1070.output_1070(1)
+    assert result == ("1\n3\n5\n7\n9\n11")
